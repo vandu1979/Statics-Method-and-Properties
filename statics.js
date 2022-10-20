e0c0f4da6943dc13071c8eb24558af399e7aef25
vandu1979 <vandana_kothari@yahoo.com>
2022-10-19 22:55:55 -0600

    Hi

--- statics.js
+++ statics.js
@@ -1,1 +1,20 @@
+class Governor {
+ 
+   
+ static doSomething1(){
+    console.log('hi')
+ }
 
+ static doAnother(){
+    console.log('hello')
+    
+
+    }
+    static Veto(){
+        console.log('hey')
+    }
+
+}
+Governor.veto()
+Governor.doSomething1()
+Governor.doAnother()
